7841e3a81a9a918e2589bc0e7f68ab27c1f13d3a
igebhar <37857361+igebhar@users.noreply.github.com>
2018-03-28 18:10:00 -0400

    Add files via upload

---
+++ filter.h
@@ -0,0 +1,18 @@
+#ifndef FILTER_H
+#define FILTER_H
+#include <iostream>
+#include <string>
+#include "Pixel.h"
+#include "Header.h"
+#include "Image.h"
+
+using namespace std;
+using Matrix = vectr<vector<int>>;
+
+namespace Filter{
+  extern Matrix K3,K5;
+  Image& sharpen(Image&, Matrix&);
+  Pixel apply_kernel(Image&, int x, int y, Matrix&);
+  int clampt(int, int int);
+}
+#endif
