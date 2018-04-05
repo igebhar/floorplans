2c1c7566a2dcebd3dcb37e620c97fd40be843273
darecluval <37857579+darecluval@users.noreply.github.com>
2018-04-05 09:26:51 -0400

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -2,7 +2,7 @@
 
 /**
 Clare DuVal and Isabella Gebhart
-CPSC 002, 001 Spring 2018
+CPSC 002, 002 Spring 2018
 ckduval, igehbar
 **/
 #include "Image.h"
@@ -21,55 +21,53 @@ Image::~Image () { // Don't allow memory leaks!
 }
 
 Header Image::read_header (ifstream& in) {
-  string magic;
-  int w, h, mc;
+  	string magic;
+  	int w, h, mc;
 
-  in >> magic;
-  Image::ignore_comments(in);
-  in >> w;
-  Image::ignore_comments(in);
-  in >> h;
-  Image::ignore_comments(in);
-  in >> mc;
-  in.ignore(256, '\n');
-  return Header(magic, w, h, mc);
+  	in >> magic;
+  	Image::ignore_comments(in);
+  	in >> w;
+  	Image::ignore_comments(in);
+  	in >> h;
+  	Image::ignore_comments(in);
+  	in >> mc;
+  	in.ignore(256, '\n');
+  	return Header(magic, w, h, mc);
 }
 
 void Image::ignore_comments (ifstream& in) {
-  char c;
-  in.get(c);
+  	char c;
+  	in.get(c);
 
-  while ( isspace(c) || c == '#') {
-    if (c == '#')
-      in.ignore(256, '\n');
-    in.get(c);
-  }
-
-  in.unget();
+  	while ( isspace(c) || c == '#') {
+   		if (c == '#')
+      		in.ignore(256, '\n');
+    		in.get(c);
+  	}
+	in.unget();
 }
 
 // This function allocates memory!
 vector<Pixel> Image::read_pixels (const Header& hdr, ifstream& in) {
-  vector<Pixel> pixels;
- int num_pixels = hdr.width() * hdr.height();
+  	vector<Pixel> pixels;
+ 	int num_pixels = hdr.width() * hdr.height();
 
-  if (hdr.magic() == "P3") {
-    uint r,g,b;
-    for (int i = 0; i < num_pixels; i++) {
-      in >> r >> g >> b;
-      pixels.push_back(Pixel(r,g,b));
-    }
-  } else {
-    uint8_t r,g,b;
-    for (int i = 0; i < num_pixels; i++) {
-      r = in.get();
-      g = in.get();
-      b = in.get();
-      pixels.push_back(Pixel(r,g,b));
-    }
-  }
-
-  return pixels;
+  	if (hdr.magic() == "P3") {
+    		uint r,g,b;
+    		for (int i = 0; i < num_pixels; i++) {
+      			in >> r >> g >> b;
+      			pixels.push_back(Pixel(r,g,b));
+    		}
+  	} else {
+    		uint8_t r,g,b;
+    		for (int i = 0; i < num_pixels; i++) {
+      			r = in.get();
+      			g = in.get();
+      			b = in.get();
+      			pixels.push_back(Pixel(r,g,b));
+    		}
+  	}
+	return pixels;
 }
 
 // accessors
@@ -89,39 +87,39 @@ void Image::write_header (ofstream& out) const {
 }
 
 void Image::write_to (ofstream& out) const {
-  write_header(out);
+	write_header(out);
 
-  int num_pixels = this->HDR.width() * this->HDR.height();
+  	int num_pixels = this->HDR.width() * this->HDR.height();
 
-  //Writes a P3
-  if (this->HDR.magic() == "P3") {
-    for (int i = 0; i < num_pixels; i++) {
-       Pixel p = this->pixel.at(i);
-      out << (int) p.r() << ' '
-          << (int) p.g() << ' '
-          << (int) p.b() << ' ';
-    }
-  } else { //Writes a P6
-    for (int i = 0; i < num_pixels; i++) {
-      Pixel p = this->pixel.at(i);
-      out << p.r() << p.g() << p.b();
-    }
-  }
+  	//Writes a P3
+  	if (this->HDR.magic() == "P3") {
+    		for (int i = 0; i < num_pixels; i++) {
+       			Pixel p = this->pixel.at(i);
+      			out << (int) p.r() << ' '
+          		<< (int) p.g() << ' '
+          		<< (int) p.b() << ' ';
+    		}
+  	} else { //Writes a P6
+    		for (int i = 0; i < num_pixels; i++) {
+      			Pixel p = this->pixel.at(i);
+      			out << p.r() << p.g() << p.b();
+    		}
+  	}
 }
 
 // This function is important!
 //Assignment operator
 Image& Image::operator=(const Image& rhs) {
-  if (this == &rhs) return *this; // Why do we need this? Hint: delete[]
-  // Header is simple
-  this->HDR = rhs.HDR;  // Assignment operator
+  	if (this == &rhs) return *this;
+  	// Header is simple
+  	this->HDR = rhs.HDR;  // Assignment operator
 
-  this->pixel = rhs.pixel;
-  return *this;
+  	this->pixel = rhs.pixel;
+  	return *this;
 }
 
 // Get one pixel
 Pixel& Image::operator() (int x, int y) {
-  int ndx = (this->HDR.width() * y) + x;
-  return this->pixel.at(ndx);
+  	int ndx = (this->HDR.width() * y) + x;
+  	return this->pixel.at(ndx);
 }
